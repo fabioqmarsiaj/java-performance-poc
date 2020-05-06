338e3ff7c58c385e2faf35cbf0b97fa09101cfe2
Fabio <fabio.marsiaj@townsq.com.br>
2020-05-06 15:43:21 -0300

    First

---
+++ src/main/java/com/fabioqmarsiaj/javaperformancepoc/JavaPerformancePocApplication.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.javaperformancepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPerformancePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPerformancePocApplication.class, args);
+    }
+
+}
